13a11c0cf7ed590ccb76b0c6f6f9efc975dbe410
Henry Mao <henry@jenni.ai>
2024-12-14 10:34:03 +0800

    Setup badge tracking

--- app/badge/[...id]/route.ts
+++ app/badge/[...id]/route.ts
@@ -1,11 +1,25 @@
+import { posthog } from "@/lib/posthog_server"
+import { waitUntil } from "@vercel/functions"
 const badgeUrl = `https://img.shields.io/badge/smithery.ai-0%20installs-%23ea580c`
 
-export async function GET() {
+export async function GET(
+	request: Request,
+	{ params }: { params: { id: string[] } },
+) {
+	const serverId = params.id.join("/")
+	posthog.capture({
+		event: "Badge Viewed",
+		distinctId: "badge-viewed",
+		properties: {
+			serverId,
+		},
+	})
 	const response = await fetch(badgeUrl)
 	if (!response.ok) {
 		return Response.json({ error: "Failed to fetch badge" }, { status: 500 })
 	}
 	const imageBuffer = await response.arrayBuffer()
+	waitUntil(posthog.flush())
 	return new Response(imageBuffer, {
 		status: 200,
 		headers: {
@@ -13,6 +27,7 @@ export async function GET() {
 			"Access-Control-Allow-Origin": "*",
 			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
 			"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			"Cache-Control": "no-cache",
 		},
 	})
 }
