741dabd8b5a0c13b3011a40253f058a8673b0b89
Henry Mao <henry@jenni.ai>
2024-12-19 00:27:15 +0700

    Add new route

--- app/registry/servers/[...id]/route.ts
+++ app/registry/servers/[...id]/route.ts
@@ -1,7 +1,27 @@
-import { NextResponse } from "next/server"
 import { db } from "@/db"
-import { eq } from "drizzle-orm"
 import { servers } from "@/db/schema"
+import {
+	isStdioFn,
+	RegistryServerSchema,
+} from "@/lib/blacksmith/registry-types"
+import Ajv from "ajv"
+import { eq } from "drizzle-orm"
+import { NextResponse } from "next/server"
+import { z } from "zod"
+const ajv = new Ajv()
+
+// const ReturnTypeSchema = RegistryServerSchema.pick({
+// 	id: true,
+// 	name: true,
+// }).extend({
+// 	connections: z.array(
+// 		z.object({
+// 			type: z.string(),
+// 			configSchema: JSONSchemaSchema,
+// 			exampleConfig: z.any(),
+// 		}),
+// 	),
+// })
 
 export async function GET(
 	request: Request,
@@ -17,9 +37,96 @@ export async function GET(
 			return NextResponse.json({ error: "Server not found" }, { status: 404 })
 		}
 
+		// TODO: Change this once ready
 		return NextResponse.json(result)
+		// return NextResponse.json(
+		// 	ReturnTypeSchema.parse({
+		// 		...result,
+		// 		connections: RegistryServerSchema.shape.connections
+		// 			.parse(result.connections)
+		// 			.map((connection) => {
+		// 				if (isStdioFn(connection)) {
+		// 					return {
+		// 						...connection,
+		// 						type: "stdio",
+		// 					}
+		// 				}
+		// 				return connection
+		// 			}),
+		// 	}),
+		// )
 	} catch (error) {
 		console.error("Error fetching server:", error)
+		return NextResponse.json(
+			{ error: "Internal server error" },
+			{ status: 500 },
+		)
+	}
+}
+
+const RequestSchema = z.object({
+	// The type of server to instantiate
+	connectionType: z.enum(["stdio"]),
+
+	// The configuration for the server
+	config: z.record(z.unknown()),
+})
+
+export async function POST(
+	request: Request,
+	{ params }: { params: { id: string[] } },
+) {
+	try {
+		const serverId = params.id.join("/")
+		const result = await db.query.servers.findFirst({
+			where: eq(servers.id, serverId),
+		})
+
+		if (!result) {
+			return NextResponse.json({ error: "Server not found" }, { status: 404 })
+		}
+
+		const { data, error } = RequestSchema.safeParse(await request.json())
+
+		if (error) {
+			return NextResponse.json({ error: error.errors }, { status: 400 })
+		}
+
+		const connections = RegistryServerSchema.shape.connections.parse(
+			result.connections,
+		)
+		// Find the right connection
+		const connection = connections.find((connection) => {
+			if (isStdioFn(connection) && data.connectionType === "stdio") {
+				return true
+			}
+			return false
+		})
+
+		// TODO: Temporary.
+		if (!connection || !isStdioFn(connection)) {
+			return NextResponse.json(
+				{ error: "Connection not found" },
+				{ status: 404 },
+			)
+		}
+
+		// Initializes configuration
+		const validate = ajv.compile(connection.configSchema)
+		const valid = validate(data.config)
+
+		if (!valid) {
+			return NextResponse.json({ error: validate.errors }, { status: 400 })
+		}
+
+		// Applies configuration
+		// biome-ignore lint/security/noGlobalEval: <explanation>
+		const stdioFunction = eval(connection.stdioFunction)
+		const finalResult = stdioFunction(data.config)
+
+		return NextResponse.json(finalResult)
+	} catch (error) {
+		console.error("Error generating config for server:", error)
 		return NextResponse.json(
 			{ error: "Internal server error" },
 			{ status: 500 },
