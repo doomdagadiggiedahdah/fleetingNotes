96413745332178346ac0fbdf554b081d932e7306
Henry Mao <1828968+calclavia@users.noreply.github.com>
2024-12-23 10:37:16 +0700

    Update Anthropic package

--- lib/blacksmith/anthropic.ts
+++ lib/blacksmith/anthropic.ts
@@ -1,11 +1,14 @@
-import type { LangfuseTraceClient } from "langfuse"
 import type Anthropic from "@anthropic-ai/sdk"
-import type { MessageCreateParams } from "@anthropic-ai/sdk/resources/index.js"
+import type {
+	MessageCreateParamsNonStreaming,
+	MessageParam,
+} from "@anthropic-ai/sdk/resources/messages/messages.mjs"
+import type { LangfuseTraceClient } from "langfuse"
 
 export async function tracedAnthropicGenerate(
 	client: Anthropic,
 	trace: LangfuseTraceClient,
-	params: MessageCreateParams,
+	params: MessageCreateParamsNonStreaming,
 ) {
 	const generation = trace.generation({
 		name: "anthropic-chat-completion",
@@ -27,4 +30,24 @@ export async function tracedAnthropicGenerate(
 	})
 
 	return response
+}
+
+export function cacheLastMessage(messages: MessageParam[]): MessageParam[] {
+	const cachedMessages: MessageParam[] = JSON.parse(JSON.stringify(messages))
+	const lastMessage = cachedMessages.at(-1)
+	if (!lastMessage) return messages
+
+	const content = lastMessage?.content
+	if (Array.isArray(content)) {
+		content[0].cache_control = { type: "ephemeral" }
+	} else if (typeof lastMessage.content === "string") {
+		lastMessage.content = [
+			{
+				type: "text",
+				text: lastMessage.content,
+				cache_control: { type: "ephemeral" },
+			},
+		]
+	}
+	return cachedMessages
 }
