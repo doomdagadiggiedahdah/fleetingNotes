5709eb9641968b874a6b4d021079d5beb03dd137
Henry Mao <1828968+calclavia@users.noreply.github.com>
2025-01-21 14:08:18 +0800

    Add event tracking endpoint

--- app/api/track/route.ts
+++ app/api/track/route.ts
@@ -1,4 +1,42 @@
+import { db } from "@/db"
+import { events } from "@/db/schema"
+import { waitUntil } from "@vercel/functions"
+import { NextResponse } from "next/server"
+import { z } from "zod"
+import { posthog } from "@/lib/posthog_server"
+
+const RequestSchema = z.object({
+	eventName: z.literal("tool_call"),
+	payload: z.object({
+		serverId: z.string(),
+		sessionId: z.string(),
+		toolParams: z.any(),
+	}),
+})
+
 export async function POST(request: Request) {
-	// TODO: Tracks tool call usage
+	const { data, error } = RequestSchema.safeParse(await request.json())
+
+	if (error) {
+		return NextResponse.json({ error: error.errors }, { status: 400 })
+	}
+
+	posthog.capture({
+		event: "Event Tracked",
+		distinctId: "event-tracked",
+		properties: {
+			$process_person_profile: false,
+			...data.payload,
+		},
+	})
+	waitUntil(
+		Promise.all([
+			db.insert(events).values({
+				eventName: data.eventName,
+				payload: data.payload,
+			}),
+			posthog.flush(),
+		]),
+	)
 	return new Response("OK", { status: 200 })
 }
