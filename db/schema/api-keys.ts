a6572205f3b68a6b3deda2154adab3d467ea8053
Henry Mao <1828968+calclavia@users.noreply.github.com>
2025-03-05 23:03:41 +0800

    Add ID to API key table

--- db/schema/api-keys.ts
+++ db/schema/api-keys.ts
@@ -2,7 +2,8 @@ import { pgTable, timestamp, uuid } from "drizzle-orm/pg-core"
 import { authUsers } from "drizzle-orm/supabase"
 
 export const apiKeys = pgTable("api_keys", {
-	key: uuid("api_key").primaryKey().defaultRandom(),
+	id: uuid("id").primaryKey().defaultRandom(),
+	key: uuid("api_key").notNull().unique().defaultRandom(),
 	owner: uuid("owner").references(() => authUsers.id, { onDelete: "cascade" }),
 	timestamp: timestamp("timestamp").notNull().defaultNow(),
 }).enableRLS()
